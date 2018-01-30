0029e43058091e5f950229bb59c7d7d2c1e8dba5
pizzyflavin <pizzyflavin@gmail.com>
2018-01-29 23:15:34 -0500

    Add init script to call other scripts, update README

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Initialization script for NVIDIA mining setups on Linux
+
+# Start logging GPU data (This is the same info that is output to the terminal
+# when the miner is run)
+./monitor_GPUs.sh
+
+# Set power limits and fan settings.
+./init_GPUs.sh
+
+# Apply overclocking to cards
+./overclock.sh
+
