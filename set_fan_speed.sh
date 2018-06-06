ee2ef623a884d35abfe16ac5f1c0d4a026d48a91
pizzyflavin <pizzyflavin@gmail.com>
2018-06-05 22:55:27 -0400

    Change fan speed to 80%, use shell variable for fanspeed

--- set_fan_speed.sh
+++ set_fan_speed.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-nvidia-settings -c :0 -a '[fan:0]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:1]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:2]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:3]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:4]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:5]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:6]/GPUTargetFanSpeed=75'
-nvidia-settings -c :0 -a '[fan:7]/GPUTargetFanSpeed=75'
+FANSPEED=80
+
+nvidia-settings -c :0 -a "[fan:1]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:1]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:2]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:3]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:4]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:5]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:6]/GPUTargetFanSpeed=$FANSPEED"
+nvidia-settings -c :0 -a "[fan:7]/GPUTargetFanSpeed=$FANSPEED"
