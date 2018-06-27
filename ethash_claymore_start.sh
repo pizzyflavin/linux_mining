44c4696eaec2a3210256180877e4a9e2d2462cc9
pizzyflavin <pizzyflavin@gmail.com>
2018-06-26 21:37:38 -0400

    Update ethash script

--- ethash_claymore_start.sh
+++ ethash_claymore_start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
+MINER=./miners/claymore/claymore11.8/ethdcrminer64
+POOL=us-east.ethash-hub.miningpoolhub.com:20535
+USERNAME=pizzyflavin
+WORKER=SirNose
 
-# Set environment variables
-export GPU_FORCE_64BIT_PTR=0
-export GPU_MAX_HEAP_SIZE=100
-export GPU_USE_SYNC_OBJECTS=1
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_SINGLE_ALLOC_PERCENT=100
-# Investigate -tt 68 and -allpools 1 options suggested on r/EtherMining
-/usr/local/claymore10/ethdcrminer64 -epool eth-us-east1.nanopool.org:9999 -ewal 0x945737e70d4fe95f6a8abf6a49d728f0b3c42c61.SirNose/pizzyflavin@gmail.com -epsw x -mode 1 -ftime 10
+screen -d -m -S eth $MINER -epool $POOL -ewal $USERNAME.$WORKER \
+    -eworker $USERNAME.$WORKER -esm 2 -epsw x -tt 80
